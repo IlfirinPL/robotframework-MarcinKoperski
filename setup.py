3c1303902a4e40b68ee4ffb2c5c4af225f04cd71
IlfirinPL <IlfirinPL@users.noreply.github.com>
2015-10-12 15:27:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2015 Cutting Edge QA
+
+
+"""Setup script for Robot's Marcin Koperski bundle distributions"""
+
+from setuptools import setup
+
+version = u'0.0.1'
+
+setup(
+    name=u'robotframework-MarcinKoperski',
+    version=version,
+    description=u'RobotFramework Marcin Koperski bundle',
+    author=u'Marcin Koperski',
+    author_email=u'marcin.koperski+github[at].gmail.com',
+    url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski',
+    download_url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski/tarball/v{version}'.format(version=version),
+    keywords=['robotframework', 'pyral'],
+    package_dir={u'': 'src'},
+    packages=['MarcinKoperski'],
+    install_requires=[
+        u'robotframework',
+        u'robotframework-archivelibrary',
+        u'robotframework-databaselibrary',
+        u'robotframework-imaplibrary',
+        u'robotframework-pabot',
+        u'robotframework-ride',
+        u'robotframework-selenium2library',
+        u'robotframework-testmanagement',
+        u'robotframework-sshlibrary',
+        u'robotframework-sudslibrary'
+    ]
+)
