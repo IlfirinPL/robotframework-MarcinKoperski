cf2ce4a1fee27e85237c442c84ce9448bc790d4e
IlfirinPL <IlfirinPL@users.noreply.github.com>
2015-10-19 10:35:01 +0200

    clean up

--- src/TestToolsMK/__init__.py
+++ src/TestToolsMK/__init__.py
@@ -20,16 +20,17 @@ class TestToolsMK:
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     ROBOT_LIBRARY_VERSION = VERSION
     
-    def __init__(self, file='Resources/googlesheet-key.json', id='1RBVyDC-HrUc5Ct0gHU0mlkfhHErmdPz0DJJgGT_XQ_Y',worksheetName='Accounts'):
-         json_key = json.load(open(file))
-         scope = ['https://spreadsheets.google.com/feeds']
-         credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
-         gc = gspread.authorize(credentials)
-         sht1 = gc.open_by_key(id)
-         self.WORKSHEET = sht1.worksheet(worksheetName)
-         self.login_list = self.WORKSHEET.col_values(1)
-         self.password_list = self.WORKSHEET.col_values(2)
-         self.dictionary = dict(zip(self.login_list, self.password_list))
+    def __init__(self, keyJsonFile=None, id=None,worksheetName=None):
+         if keyJsonFile!=None:
+            json_key = json.load(open(keyJsonFile))
+            scope = ['https://spreadsheets.google.com/feeds']
+            credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+            gc = gspread.authorize(credentials)
+            if id != None:
+                sht1 = gc.open_by_key(id)
+                if worksheetName != None:
+                    self.WORKSHEET = sht1.worksheet(worksheetName)
+
 
     def get_spreadsheet_by_id(self, file, id):
          json_key = json.load(open(file))
@@ -41,9 +42,15 @@ class TestToolsMK:
     def select_worksheet_by_name(self, worksheet_name):
         self.WORKSHEET = self.SPREADSHEET.worksheet(worksheet_name)
     def get_dictonary_logins_and_passwords(self):
+         self.login_list = self.WORKSHEET.col_values(1)
+         self.password_list = self.WORKSHEET.col_values(2)
+         self.dictionary = dict(zip(self.login_list, self.password_list))
         return self.dictionary
     def get_password_for_login(self,login):
        """Return password for provided login, rise error when login is missing"""
+         self.login_list = self.WORKSHEET.col_values(1)
+         self.password_list = self.WORKSHEET.col_values(2)
+         self.dictionary = dict(zip(self.login_list, self.password_list))
        return self.dictionary[login] 
     def find_cell_using_regex(self,regex):
        """Return password for provided login, rise error when login is missing"""
