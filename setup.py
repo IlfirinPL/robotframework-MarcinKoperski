7d34bf135fb397fdf1e28bf865d39ef507b4c755
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2016-04-10 15:07:24 +0200

    remove dependency from setup

--- setup.py
+++ setup.py
@@ -18,24 +18,24 @@ __version_file_path__ = os.path.join(src_path, 'TestToolsMK', 'VERSION')
 __version__ = open(__version_file_path__, 'r').read().strip()
 
 setup(
-    name=u'robotframework-MarcinKoperski', version=__version__,
-    description=u'RobotFramework Marcin Koperski bundle',
-    author=u'Marcin Koperski',
-    author_email=u'marcin.koperski+github[at].gmail.com', license='AGPL',
+    name='robotframework-MarcinKoperski', version=__version__,
+    description='RobotFramework Marcin Koperski bundle',
+    author='Marcin Koperski',
+    author_email='marcin.koperski+github[at].gmail.com', license='AGPL',
     url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski',
     download_url=u'https://github.com/IlfirinPL/robotframework-MarcinKoperski/archive/master.zip'.format(version=__version__), keywords=['robotframework'],
     package_dir={u'': 'src'}, packages=['TestToolsMK'], exclude_package_data={'': ['.git', '.git/*', '.idea', '.gitignore', 'doc/*', 'atest/*']},
     package_data={'TestToolsMK': ['VERSION']},
     install_requires=[
-        u'robotframework>=3.0',
-        u'robotframework-archivelibrary', u'robotframework-databaselibrary', u'robotframework-imaplibrary', u'robotframework-pabot',
-        u'robotframework-ride>=1.5.2.1',
-        u'robotframework-selenium2library>=1.7.4', u'robotframework-testmanagement>=0.1.10', # u'robotframework-sshlibrary>=2.1.1',
-        u'robotframework-sudslibrary',
-        u'robotframework-appiumlibrary',
-        u'gspread', u'unicodecsv', u'oauth2client',
-        u'selenium',
-        u'robotframework-httplibrary'
+        'robotframework>=3.0',
+        'robotframework-archivelibrary', 'robotframework-databaselibrary', 'robotframework-imaplibrary', 'robotframework-pabot',
+        'robotframework-ride>=1.5.2.1',
+        'robotframework-selenium2library', u'robotframework-testmanagement',  # u'robotframework-sshlibrary>=2.1.1',
+        'robotframework-sudslibrary',
+        'robotframework-appiumlibrary',
+        'gspread', 'unicodecsv', 'oauth2client',
+        'selenium',
+        'robotframework-httplibrary', 'lxml'
 
     ]
 )
