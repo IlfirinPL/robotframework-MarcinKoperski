b21ca169098913e107efdd0980a2fa41e1cb53a0
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2018-12-18 13:53:40 +0100

    install all script for python3

---
+++ utils/install-all.bat
@@ -0,0 +1,6 @@
+set PY_PYTHON=3 
+py -m pip install -U pip wheel pywin32
+py -m pip install -U -r https://raw.githubusercontent.com/IlfirinPL/robotframework-MarcinKoperski/master/requirements.txt
+py -m pip install -U git+https://github.com/IlfirinPL/robotframework-MarcinKoperski.git
+py -m pip install -U -r https://github.com/HelioGuilherme66/RIDE/raw/release/requirements.txt
+py -m pip install -U git+https://github.com/HelioGuilherme66/RIDE.git
