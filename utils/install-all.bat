b07f9dc2525f03c3d3824752686c5a7347e3ff1d
Marcin Koperski <IlfirinPL@users.noreply.github.com>
2019-10-17 10:47:31 +0200

    Update install-all.bat

--- utils/install-all.bat
+++ utils/install-all.bat
@@ -2,5 +2,5 @@ set PY_PYTHON=3
 py -m pip install -U pip wheel pywin32 setuptools
 py -m pip install -U -r https://raw.githubusercontent.com/IlfirinPL/robotframework-MarcinKoperski/master/requirements.txt
 py -m pip install -U git+https://github.com/IlfirinPL/robotframework-MarcinKoperski.git
-py -m pip install -U robotframework-ride
+py -m pip install -U git+https://github.com/robotframework/RIDE.git
 py -m robot --version
